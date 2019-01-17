98384e27cf2fb18950de9d8ca6d1adc8077991c5
yybawang <512511253@qq.com>
2019-01-17 10:56:11 +0800

    基本功能完成

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # assh
 自动登录ssh脚本，使用 expect ，优化操作和加入配置文件
+
+### 第一步，需先安装 expect
+```bash
+sudo apt install -y expect
+``` 
+
+centos/redhat 系列需先安装
+
+```bash
+sudo yum instal -y expect
+```
+
+> 其他发行版自行使用包管理安装 `expect` 包
+
+### 第二步，配置服务器连接信息
+配置文件位于项目目录/ssh_hosts.conf，里面有注释说明以及配置格式
+
+### 第三步，运行脚本选择服务器进行连接
+```bash
+cd 此项目目录
+./assh.sh
+# 输入序号进行连接(直接回车默认0)
+# 如无执行权限，请先  chmod +x run.sh expect.sh
+```
+### 第四部，配置alias，简化操作
+```bash
+vi ~/.bashrc
+# 最后一行加入
+alias assh='/此项目绝对路径/assh.sh'
+# 示例
+# alias assh='/var/www/assh/assh.sh'
+
+# 即时生效
+source ~/.bashrc
+```
---
+++ expect.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/expect
+set user [lindex $argv 0]
+set ip [lindex $argv 1]
+set port [lindex $argv 2]
+set password [lindex $argv 3]
+set home [lindex $argv 4]
+set timeout 10
+if { [regexp -nocase "/" $password] } {
+	spawn ssh -o ServerAliveInterval=60 -p ${port} -i ${password} ${user}@${ip}
+} else {
+	spawn ssh -o ServerAliveInterval=60 -p ${port} ${user}@${ip}
+}
+
+# 判断条件，发送交互数据,
+expect {
+	timeout {puts "timeout"; exit}
+	"*yes/no"	{send "yes\r"; exp_continue}
+	"*password:" {send "${password}\r"; exp_continue}
+	"*?ast login" {send "cd ${home}\r"}
+}
+# 启用交互
+interact
